29f4997483a2cf807cd1f97f916ed5c3e90071a3
taeo920 <taeo920@gmail.com>
2015-06-13 02:52:03 -0400

    Added additional meta tags including fb share image and favicon

--- app/wp-content/themes/smorto/partials/head.php
+++ app/wp-content/themes/smorto/partials/head.php
@@ -4,11 +4,23 @@
 <!--[if (gt IE 9)|!(IE)]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<title><?php wp_title('|'); ?></title>
 	<meta name="HandheldFriendly" content="True">
 	<meta name="MobileOptimized" content="320">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="cleartype" content="on">
+    
+    <title><?php wp_title('|'); ?></title>
+    <meta name="author" content="Steve Morton">	
+    <meta name="description" content="Portfolio and interactive resume of Steve Morton, a Web Developer in Baltimore, MD specializing in PHP CMS development.">
+    <meta name="keywords" content="Steve Morton, Web Developer, WordPress, PHP, CMS">
+    
+    <meta property="og:image" content="<?php bloginfo('template_url'); ?>/images/fb-share.png">
+    <meta property="og:title" content="<?php wp_title('|'); ?>">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="Portfolio and interactive resume of Steve Morton, a Web Developer in Baltimore, MD specializing in PHP CMS development.">
+    
+    <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.png" type="image/png">
+    <link rel="icon" href="<?php bloginfo('template_url'); ?>/images/favicon.png" type="image/png">
 
 	<?php wp_head(); ?>
 
