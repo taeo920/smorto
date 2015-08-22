a70c5c6e08f0b22a176833c78ba5337b2c7151c5
taeo920 <taeo920@gmail.com>
2015-08-22 07:15:24 -0400

    Resolved js issue due to structure change

--- wp-content/themes/smorto/partials/header.php
+++ wp-content/themes/smorto/partials/header.php
@@ -2,10 +2,10 @@
     <div class="container">
         <nav class="menu">
             <ul class="menu-list nav">
-                <li class="menu-link work"><a href="<?php bloginfo('url'); ?>#work">Work</a></li>
-                <li class="menu-link experience"><a href="<?php bloginfo('url'); ?>#experience">Experience</a></li>
-                <li class="menu-link skills"><a href="<?php bloginfo('url'); ?>#skills">Skills</a></li>
-                <li class="menu-link contact"><a href="<?php bloginfo('url'); ?>#contact">Contact</a></li>
+                <li class="menu-link work"><a href="#work">Work</a></li>
+                <li class="menu-link experience"><a href="#experience">Experience</a></li>
+                <li class="menu-link skills"><a href="#skills">Skills</a></li>
+                <li class="menu-link contact"><a href="#contact">Contact</a></li>
             </ul>
         </nav>
     </div>
